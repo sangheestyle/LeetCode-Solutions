3bd3151b81ae30fb212e77d4f675594f26744773
Sanghee Kim <sangheestyle@gmail.com>
2019-02-12 20:17:10 +0200

    update solution

--- Python/next-closest-time.py
+++ Python/next-closest-time.py
@@ -12,7 +12,7 @@ class Solution(object):
         result = None
         for i in xrange(curr+1, curr+1441):
             t = i % 1440
-            h, m = t // 60, t % 60
+            h, m = divmod(t, 60)
             result = "%02d:%02d" % (h, m)
             if set(result) <= set(time):
                 break
